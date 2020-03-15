bde8ca169e646ee9da2c64f433e0fa7c7db9445a
jmgorton <jgorton23@gmail.com>
2020-03-15 17:38:38 -0500

    initial commit

---
+++ force-app/main/default/lwc/acctFields/acctFields.js
@@ -0,0 +1,23 @@
+// export default class AcctFields extends LightningElement {}
+
+import { LightningElement } from 'lwc';
+export default class HelloIteration extends LightningElement {
+    @track
+    contacts = [
+        {
+            Id: 1,
+            Name: 'Amy Taylor',
+            Title: 'VP of Engineering',
+        },
+        {
+            Id: 2,
+            Name: 'Michael Jones',
+            Title: 'VP of Sales',
+        },
+        {
+            Id: 3,
+            Name: 'Jennifer Wu',
+            Title: 'CEO',
+        },
+    ];
+}
