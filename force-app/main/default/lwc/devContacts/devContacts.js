85a2a55546c3a2c6ee82c573df127d37960aafdf
jmgorton <jgorton23@gmail.com>
2020-03-17 18:49:54 -0500

    revert nesting cards. retrieve contact data

--- force-app/main/default/lwc/devContacts/devContacts.js
+++ force-app/main/default/lwc/devContacts/devContacts.js
@@ -1,9 +1,10 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
 
 // import getContactList from "@salesforce/apex/ContactController.getContactList";
+import getDevContactList from "@salesforce/apex/ContactController.getDevContactList";
 // import getContactList from '/scripts/soql/account.soql';
 
 export default class DevContacts extends LightningElement {
-//   @wire(getContactList) contacts;
-    contacts;
+  @wire(getDevContactList) devcontacts;
+    // devcontacts;
 }
