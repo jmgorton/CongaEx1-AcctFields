7a4787a9c2ba7e04eb684f8aa23d13acc849a6b2
jmgorton <jgorton23@gmail.com>
2020-03-17 19:31:44 -0500

    retrieve page specific acct info

--- force-app/main/default/lwc/acctFields/acctFields.js
+++ force-app/main/default/lwc/acctFields/acctFields.js
@@ -1,4 +1,5 @@
-import { LightningElement, track, wire } from "lwc";
+import { LightningElement, track, wire, api } from "lwc";
+import { getRecord } from 'lightning/uiRecordApi';
 import getContactList from "@salesforce/apex/ContactController.getContactList";
 import getDevContactList from "@salesforce/apex/ContactController.getDevContactList";
 import getCustSuccessContactList from "@salesforce/apex/ContactController.getCustSuccessContactList";
@@ -8,8 +9,48 @@ import getCustSuccessContactList from "@salesforce/apex/ContactController.getCus
 // var records;
 // var records1;
 
+const FIELDS = [
+    'Contact.Name',
+    'Contact.Title',
+    'Contact.Phone',
+    'Contact.Email',
+];
+
+const FIELDS2 = [
+    'Account.Name',
+    'Account.Id'
+];
+
 export default class AcctFields extends LightningElement {
   // export default class HelloIteration extends LightningElement {
+      @api recordId;
+
+      @wire(getRecord, { recordId: '$recordId', fields: FIELDS }) contact;
+      @wire(getRecord, { recordId: '$recordId', fields: FIELDS2 }) account;
+
+    get name() {
+        return this.contact.data.fields.Name.value;
+    }
+
+    get title() {
+        return this.contact.data.fields.Title.value;
+    }
+
+    get phone() {
+        return this.contact.data.fields.Phone.value;
+    }
+
+    get email() {
+        return this.contact.data.fields.Email.value;
+    }
+
+    get acctName() {
+        return this.account.data.fields.Name.value;
+    }
+
+    get id() {
+        return this.account.data.fields.Id.value;
+    }
 
   // @track
   // contacts = [
